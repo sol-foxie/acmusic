3dfead75a9e0eeef32e29df39495f09347f98b2d
sol_foxie <sol@foxie.moe>
2019-01-09 18:11:03 -0500

    remove spinner

--- app/src/main/java/moe/foxie/sol/acmusic/MainActivity.kt
+++ app/src/main/java/moe/foxie/sol/acmusic/MainActivity.kt
@@ -7,16 +7,14 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
-import android.view.View
-import android.widget.AdapterView
+
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 import android.Manifest
 
 /**
  * the Activity that executes when this app is launched.
  */
-class MainActivity : Activity(), AdapterView.OnItemSelectedListener, MusicPlayerService.ServiceListener {
+class MainActivity : Activity(), MusicPlayerService.ServiceListener {
 
     private var service: MusicPlayerService? = null
         set(value) {
@@ -50,8 +48,6 @@ class MainActivity : Activity(), AdapterView.OnItemSelectedListener, MusicPlayer
         this.startForegroundService(onlineIntent)
         setContentView(R.layout.activity_main)
 
-        jukebox.onItemSelectedListener = this
-
         playPause.setOnClickListener{
             service?.manager?.playPause()
         }
@@ -70,31 +66,7 @@ class MainActivity : Activity(), AdapterView.OnItemSelectedListener, MusicPlayer
 
     override fun update(trackID: Int, state: MusicManager.State) {
         this.runOnUiThread {
-            jukebox.setSelection(trackID)
             playPause.setText(state.uiPlayPauseString())
         }
     }
-
-    /**
-     * a Spinner fires a selection when loaded, even if the user didn't interact with it.
-     * it's unfortunate, but we need to suppress this and this kludge seems like the best way.
-     */
-    private var initialSelection = true
-    /**
-     * plays a track as selected from a Spinner view by the user.
-     * assumes the listing of tracks in the Spinner are arranged in chronological order,
-     * such that 12am is at row 0 and 11pm is at row 23.
-     * does not do anything if the track selected is currently being played by the MusicManager.
-     * todo: decide whether selecting an item in the Spinner while the MusicManager is paused should make it play
-     */
-    override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, pos: Int, row: Long) {
-        require(adapterView!!.id == jukebox.id)
-        //if (!initialSelection) service?.manager?.changeTrackID(row.toInt())
-        initialSelection = false
-    }
-
-    //todo: figure out if this even gets called when the view in question is a Spinner
-    override fun onNothingSelected(adapterView: AdapterView<*>?) {
-    }
-
 }
