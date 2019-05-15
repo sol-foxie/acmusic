ec2eb132e4d2ced47baae90dad089829c39fe9c8
sol_foxie <sol@foxie.moe>
2019-05-14 23:12:22 -0400

    remove redundant actionBar.setDisplayHomeAsUpEnabled()

--- app/src/main/java/moe/foxie/sol/acmusic/Settings.kt
+++ app/src/main/java/moe/foxie/sol/acmusic/Settings.kt
@@ -15,7 +15,6 @@ class Settings : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        actionBar.setDisplayHomeAsUpEnabled(true)
 
         prefs = getSharedPreferences(PREFERENCES,Context.MODE_PRIVATE)
         listener = SharedPreferences.OnSharedPreferenceChangeListener { prefs, key -> if (key == SOUNDTRACK_PREFERENCE) updateSelectedButton() }
